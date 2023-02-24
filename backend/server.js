8974ff924c4be0d2f3732b8860dab94d93da9b74
saminoorsyed <saminoorsyed@gmail.com>
2023-02-24 09:10:25 -0500

    add user registration and authentication + encryption

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,20 @@ const express = require('express');
 const dotenv = require('dotenv');
 const {chats} = require("./data/dummyData");
 const connectDB = require('./config/db');
-const colors = require('colors')
+const colors = require('colors'); //makes listening output stand out
+const userRoutes = require('./routes/userRoutes');
+
 dotenv.config();
 connectDB()
 const app = express();
+
+app.use(express.json()); // allows server to accept json data
 
 app.get('/', (req, res) =>{
     res.send("api is Running");
 });
 
-app.get('/api/chat', (req, res)=>{
-    res.send(chats);
-});
+app.use('/api/user', userRoutes);
 
-app.get("/api/chat/:id", (req, res) =>{
-    const singleChat = chats.find((c)=> c._id === req.params.id);
-    res.send(singleChat);
-})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`server is listening on port ${PORT}`.yellow.bold));
